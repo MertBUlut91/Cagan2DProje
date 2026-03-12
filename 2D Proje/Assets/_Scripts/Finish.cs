299c281052b6e2abc37627eadaff771a1876e7b9
MertBUlut91 <90782690+MertBUlut91@users.noreply.github.com>
2026-03-12 19:04:03 +0300

    Level

--- 2D Proje/Assets/_Scripts/Finish.cs
+++ 2D Proje/Assets/_Scripts/Finish.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 public class Finish : MonoBehaviour
 {
     private bool levelCompleted = false;
+    [SerializeField]private GameObject finishVFX ;
+    [SerializeField] private Animator anim;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player") && !levelCompleted)
         {
             levelCompleted = true;  
+            finishVFX.SetActive(true);
+            anim.SetBool("End",true);
             
             Invoke("ComplateLevel",2f);
         }
